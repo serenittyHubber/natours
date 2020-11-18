a3219860b78b131718b4c16342cec9f0c7071ff1
José Carlos <carlos.avellar@gmail.com>
2020-11-18 18:47:16 -0300

    Commit lint testw

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,6 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
-	rules: [
-		'lower-case', // default
-		'upper-case', // UPPERCASE
-		'camel-case', // camelCase
-		'kebab-case', // kebab-case
-		'pascal-case', // PascalCase
-		'sentence-case', // Sentence case
-		'snake-case', // snake_case
-		'start-case', // Start Case
-	],
+	rules: {
+		'hello-world-rule': [2, 'always'],
+	},
 };
