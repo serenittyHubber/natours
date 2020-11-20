4d81436b3644bdf611f901077b47a46277e882a3
José Carlos <carlos.avellar@gmail.com>
2020-11-20 11:04:36 -0300

    [ISSUE-8] : package.json

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-const format = require('prettier-eslint');
+// const format = require('prettier-eslint');
 const dotenv = require('dotenv');
 
 const app = require('./app');
 const mongoose = require('mongoose');
 
 dotenv.config({
-  path: 'config.env',
+    path: 'config.env',
 });
 mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 const port = 3000;
