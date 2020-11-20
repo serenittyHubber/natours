dffd8c1be59b3cd682ab01b9d9ffe1337c2e38ce
José Carlos <carlos.avellar@gmail.com>
2020-11-20 16:51:49 -0300

    [ISSUE-9] : package.json

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,7 +1,18 @@
+eslint(prettier / prettier);
 const Tour = require('./../models/tourModel.js');
 
 exports.getAllTours = async (req, res) => {
   const tours = await Tour.find();
+
+  res.status(200).json({
+    status: 'Success',
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+exports.createTour = async (req, res) => {
+  const tours = await Tour.create(req.body);
 
   res.status(200).json({
     status: 'Success',
--- src/server.js
+++ src/server.js
@@ -4,16 +4,16 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const mongoose = require('mongoose');
 
-dotenv.config({
-    path: 'config.env',
-});
+dotenv.config({ path: 'config.env' });
 mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Server started at port ${port}`));
+app.listen(port, () => {
+  return `Server started at port ${port}`;
+});
